edafffc397f32238b7ac6b892979702594ed6d31
sevenbbek <bi.ray1225@gmail.com>
2023-09-15 12:26:48 +0545

    Compete

---
+++ 01_basic/index.js
@@ -0,0 +1,13 @@
+const id = 123;
+var name = "bibek";
+let address = "Kathamdu";
+houseNo=1001;  //->variable can be declear like this but dont use this
+let friend;
+//id = 122; -> not allowed becaouse it const i.e value cant be changed
+/*
+prefer not to use var
+because of issue in block and functional scope.
+*/
+
+console.table([id, name, address, houseNo, friend]);
+console.log(id);
