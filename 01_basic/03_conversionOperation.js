0acd1688c937380198bd00820b1862d7033c8d25
sevenbbek <bi.ray1225@gmail.com>
2023-09-16 16:37:05 +0545

    03_conversionOperation
    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let age = 33
+let score = "34aa"
+
+// console.log(typeof age);
+// console.log(typeof score);
+
+let scoreInNumber = Number(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+/*
+NOTE:
+"33" => 33
+"33aa" => NaN
+true = 1; false = 0
+*/
+ 
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  
+/*
+NOTE
+1 = true
+0 = false
+bibek = true
+"" = false
+*/
+
+let number = 100;
+
+let numberToString = String(number)
+
+console.log(numberToString)
+console.log(typeof numberToString);
