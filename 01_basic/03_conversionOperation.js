6316a19eae049d11ccf6e4bc1480498a2344b91a
sevenbbek <bi.ray1225@gmail.com>
2023-09-16 19:02:16 +0545

    Update 03_conversionOperation.js
    added operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -32,5 +32,46 @@ let number = 100;
 
 let numberToString = String(number)
 
-console.log(numberToString)
-console.log(typeof numberToString);
+// console.log(numberToString)
+// console.log(typeof numberToString);
+
+
+// **************** Operation *********************
+let value = 2
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello "
+let str2 = "bibek"
+
+console.log(str1+str2);
+
+let num1 = 10
+let str3 = "20"
+// console.log("10 and str20 "+num1+str3);
+// console.log("str20 and 10 "+str3+num1);
+
+// console.log("str1+num1: "+str1+num1);
+
+// console.log("1" + 2 + 2); // 122
+// console.log(1+2+"2"); // 32
+
+// console.log(1+2+2+"2"+"5"); // 525
+// console.log(1+2+2+"2"+5); // 525
+// console.log(1+2+2+"2"+5+1); // 5251
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(-true); // -1
+// console.log(+""); // 0
+
+
+console.log("increment and decreament");
+let num2 = 2
+let num3 = num2++
+console.log(num3); //2
+
+let num4 = ++num2
+console.log(num4); //4
+
+
