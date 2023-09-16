35f209cde732f0ac01658e2ec0f8754ff5577e66
sevenbbek <bi.ray1225@gmail.com>
2023-09-16 20:35:17 +0545

    Update datatype-summary.js
    memory - stack and heap

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -13,14 +13,14 @@
  */
 
 const id = Symbol('123')
-const anotherId = Symbol('123')
+const anotherId = Symbol('123') //datatype:symbol
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
-const bigNumber = 1235455557896541338n; //n at last makes it bigInt type
+const bigNumber = 1235455557896541338n; //n at last makes it bigInt type //datatype:bigint
 
 //array
-const fruits = ["orange", "mango"]
+const fruits = ["orange", "mango"] //datatype:object
 
 //objects -> store value in key-value pair
 let obj = {
@@ -33,5 +33,25 @@ function greet(){ //datatype: function
     console.log("Namaste!");
 }
 
-greet()
-console.log(typeof greet);
+//greet()
+
+const value = null; //datatype: object
+
+// console.log("function "+typeof greet);
+// console.log("symbol "+typeof id);
+// console.log("bigInt "+typeof bigNumber);
+// console.log("object "+typeof obj);
+// console.log("array "+typeof fruits);
+// console.log("null "+typeof value);
+
+
+
+
+//++++++++++++++ MEMORY +++++++++++++++++++++
+/**
+ * stack -> primitive 
+ * (we get copy of value, so change in one value wont reflect in another)
+ * 
+ * heap -> used by non-primitive type data type 
+ * (change in one place will reflect in another too, because reference is at same place or value)
+ */
