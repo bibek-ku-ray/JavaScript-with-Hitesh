ee706d78cc67c5dccf679bb21f01237f2c6ef60d
sevenbbek <bi.ray1225@gmail.com>
2023-09-16 22:53:29 +0545

    Create 05_strings.js

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Bibek"
+const age = 21
+
+// console.log("My Name is "+name+" and my age is "+age);
+/**
+ * dont use this because it is old
+ * use below syntax
+ */
+
+console.log(`My name is ${name.toUpperCase()} and my age is ${age}.`)
+
+const gameName = new String("ray-bibek-kumar")
+
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(4));
+// console.log(gameName.slice(-6,2));
+
+// const myName = "   bibek"
+// console.log(myName.trim());
+
+// console.log(gameName.split('-'));
+// console.log(gameName.split(''));
+// console.log(gameName.concat("2555"));
+
+console.log(gameName.match('b', 'k'));
