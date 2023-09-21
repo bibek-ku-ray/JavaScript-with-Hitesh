d347aa548ee0a81fee8911b4ad655eb4d70a0564
sevenbbek <bi.ray1225@gmail.com>
2023-09-21 23:34:15 +0545

    Update five.js

---
+++ 05_iterations/five.js
@@ -0,0 +1,46 @@
+const coding = ["Cpp", "java", "c", "javascript", "ruby"]
+
+/**
+ * because it is call back function it doesnt have name
+ * value of array automatically goes to the function as argument i.e in val.
+ */
+
+// coding.forEach(function (val)  {
+//     console.log(val);
+// })
+
+
+//arrow function
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+//another way -> function can also be passed 
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const language = [
+    {
+        languageName: "Javascipt",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "ruby",
+        languageFileName: "rb"
+    }
+]
+
+language.forEach((item)=>{
+    console.log(item.languageName);
+})
