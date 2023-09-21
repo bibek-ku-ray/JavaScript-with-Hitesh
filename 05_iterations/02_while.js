3729c8a29b0e6657689135b770c1d9f0e7b1d0f6
sevenbbek <bi.ray1225@gmail.com>
2023-09-21 12:28:41 +0545

    while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,22 @@
+//while and do-while
+
+let index = 0
+while (index <= 10) {
+    console.log(`value: ${index}`);
+    index += 2
+}
+
+let arr = 0
+let superHero = ["batman", "flash", "superman","spiderman"]
+while (arr < superHero.length) {
+    console.log(`Value: ${superHero[arr]}`);
+    arr++
+}
+
+
+
+let score = 12
+do {
+    console.log(`score: ${score}`);
+    score++
+} while (score <= 10);
