66e02fe0ee847c6ebfb61c0cfe9f2094ad2ae4af
sevenbbek <bi.ray1225@gmail.com>
2023-09-21 08:46:22 +0545

    While and do-while

---
+++ 05_iterations/one.js
@@ -0,0 +1,55 @@
+// for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+    
+
+//     if(element == 5){
+//         console.log("5 is best");
+//     }
+//     console.log(element);
+    
+// } 
+
+// let count = 0
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value: Multiple of ${i}`);
+
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${j} and outer loop: ${i}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+//         count++
+//     }
+    
+// }
+// console.log(`Count: ${count}`);
+
+// let superHero = ["batman", "flash", "superman","spiderman"]
+// for (let index = 0; index < superHero.length; index++) {
+//     const element = superHero[index];
+//     console.log(element);
+    
+// }
+
+// break and continue
+// console.log(`\nBreak`);
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         break
+//     }
+//     console.log(`Value of index is: ${index}`);    
+// }
+
+
+// console.log(`\nContinue`);
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log("5 is found");
+//         continue
+//     }
+//     console.log(`Value of index is: ${index}`);
+    
+// }
+
+
+
