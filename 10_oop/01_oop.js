5a4c63639290b86ccc693f6b0d392c5aa4980b7f
sevenbbek <bi.ray1225@gmail.com>
2023-10-24 22:43:05 +0545

    OOP started

---
+++ 10_oop/01_oop.js
@@ -0,0 +1,35 @@
+const instauser = {
+    username: "bibek",
+    loginCount: 4,
+    signedIn: true,
+
+    getuserDetail: function(){
+        console.log("got user detail from database");
+        console.log(`username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getuserDetail());
+// console.log(this);
+
+function user(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greet = function(){
+        console.log(`Welcome, ${this.username}`);
+    }
+
+    return this;
+}
+
+const userOne = new user('john', 12, true);
+const userTwo = new user('cena', 4, false);
+
+console.log('userone: ', userOne);
+// console.log('usertwo: ', userTwo);
+
+// console.log(userOne.constructor);
---
+++ 10_oop/notes.md
@@ -0,0 +1,26 @@
+# Javascript and classes
+
+## OOP
+
+## Object
+
+- collection of properties and methods
+- toLowerCase
+
+## why use OOP
+
+## parts of OOP
+
+Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+## 4 pillars
+
+Abstraction -> hiding the internal detail
+Encapsulation -> wrapping up the data
+Inheritance
+Polymorphism -> many form of same things.
